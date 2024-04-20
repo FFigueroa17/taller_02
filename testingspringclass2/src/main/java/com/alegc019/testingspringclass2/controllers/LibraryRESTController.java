35966513649716c368bcf7d0c31c91b89f6fa427
AleGC019 <00083521@uca.edu.sv>
2024-04-20 11:49:03 -0600

    Change Mapping root

--- testingspringclass2/src/main/java/com/alegc019/testingspringclass2/controllers/LibraryRESTController.java
+++ testingspringclass2/src/main/java/com/alegc019/testingspringclass2/controllers/LibraryRESTController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/library")
+@RequestMapping("/libraryrest")
 public class LibraryRESTController {
 
     @Autowired
--- testingspringclass2/src/main/java/com/alegc019/testingspringclass2/domain/dtos/LoginDTO.java
+++ testingspringclass2/src/main/java/com/alegc019/testingspringclass2/domain/dtos/LoginDTO.java
@@ -7,6 +7,7 @@ import lombok.Data;
 public class LoginDTO {
 
     @NotEmpty
+
     private String identifier;
 
     @NotEmpty
